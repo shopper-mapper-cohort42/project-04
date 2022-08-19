ceead6187f7328dd623c623ea6e2f379f6c13a4a
Max <max.t.c.law@gmail.com>
2022-08-19 11:33:58 -0400

    added template axios call for PlaceSearch API to app.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,38 @@ import "./App.css";
 import Location from "./Location";
 import WelcomePage from "./WelcomePage";
 import { Link, Routes, Route } from "react-router-dom";
+import axios from "axios";
+
+const apiKey = 'SbABP9Vr89Ox8a38s29QPLUQm51xa784';
 
 function App() {
+
+  /*
+  // Place Search API Template
+
+  // Testing Parameters
+  const longitude = -79.3832;
+  const latitude = 43.6532;
+  const sort = 'relevance'
+  const radius = 10000;
+  const userQuery = 'clothing';
+
+  const placeSearchResults = axios({
+    url: `https://www.mapquestapi.com/search/v4/place`,
+    params: {
+      location: `${longitude},${latitude}`,
+      sort: sort,
+      feedback: false,
+      key: apiKey,
+      circle: `${longitude},${latitude},${radius}`,
+      pageSize: 50,
+      q: userQuery
+    }
+  }).then((response) => {
+    console.log(response.data.results)
+  })
+  */
+
   return (
     <>
       <div className="App">
