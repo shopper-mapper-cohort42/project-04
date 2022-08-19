6c51d0876fa72c6a3cbf807239fb48831540ad14
ShannonTao <shannon.cxq@gmail.com>
2022-08-19 11:20:29 -0400

    added welcome page component, and location buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Location from "./Location";
+import WelcomePage from "./WelcomePage";
+import { Link, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <>
+      <div className="App">
+        <h1>Shopper Mapper App'er</h1>
+        <h2>Average Results for Average People</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est nisi
+          voluptates perspiciatis laudantium repellat totam doloribus at
+          repellendus repudiandae delectus aspernatur ex neque reprehenderit
+          quod, consectetur, id distinctio autem velit.
+        </p>
+      </div>
+
+      <Routes>
+        <Route path={"/"} element={<WelcomePage />} />
+
+        <Route path={"/location"} element={<Location />}></Route>
+      </Routes>
+    </>
   );
 }
 
