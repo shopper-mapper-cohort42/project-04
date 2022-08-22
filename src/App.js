6f079c0e2abe45f919cf217f36e2e172e0234e7d
Akalpit <skininja673@gmail.com>
2022-08-22 19:05:57 -0400

    fixed the routing error

--- src/App.js
+++ src/App.js
@@ -1,56 +1,34 @@
-import "./App.css";
-import Location from "./Location";
-import WelcomePage from "./WelcomePage";
-import Header from "./Header";
-import { Link, Routes, Route } from "react-router-dom";
-import axios from "axios";
-import homeImage from "./assets/home-image.png";
-
+import './App.css';
+import Location from './Location';
+import WelcomePage from './WelcomePage';
+import Header from './Header';
+import { Link, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+import homeImage from './assets/home-image.png';
+import Results from './Results';
+import SearchItems from './SearchItems';
 
 function App() {
-  /*
-  // Place Search API Template
+    const apiKey = 'SbABP9Vr89Ox8a38s29QPLUQm51xa784';
 
-  // Testing Parameters
-  const longitude = -79.3832;
-  const latitude = 43.6532;
-  const sort = 'relevance'
-  const radius = 10000;
-  const userQuery = 'clothing';
+    return (
+        <>
+            <div className="App">
+                <Header />
+                <main id="mainContent">
+                    <section className="welcomePageDiv">
+                        <Routes>
+                            <Route path="/" element={<WelcomePage />} />
+                            <Route path="/location" element={<Location apiKey={apiKey} />} />
 
-  const placeSearchResults = axios({
-    url: `https://www.mapquestapi.com/search/v4/place`,
-    params: {
-      location: `${longitude},${latitude}`,
-      sort: sort,
-      feedback: false,
-      key: apiKey,
-      circle: `${longitude},${latitude},${radius}`,
-      pageSize: 50,
-      q: userQuery
-    }
-  }).then((response) => {
-    console.log(response.data.results)
-  })
-  */
-
-  return (
-    <>
-      <div className="App">
-        <Header />
-        <main id="mainContent">
-          <section className="welcomePageDiv">
-            <Routes>
-              <Route path={"/"} element={<WelcomePage />} />
-              <Route path={"/location"} element={<Location />}></Route>
-            </Routes>
-          </section>
-        </main>
-      </div>
-    </>
-  );
+                            <Route path="/location/:coords" element={<SearchItems apiKey={apiKey} />} />
+                            <Route path="/location/:coords/:searchItem" element={<Results apiKey={apiKey} />} />
+                        </Routes>
+                    </section>
+                </main>
+            </div>
+        </>
+    );
 }
 
 export default App;
-
-// apikey:SbABP9Vr89Ox8a38s29QPLUQm51xa784
