8bca2414e56cce426353eea205b180f1345300b6
ShannonTao <shannon.cxq@gmail.com>
2022-08-23 22:11:07 -0400

    restyled Welcome Page

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -3,17 +3,20 @@ import { Link } from "react-router-dom";
 function WelcomePage() {
   return (
     <>
-      <div className="subHeaderBox">
-        <h2>Average Results for Average People</h2>
+      <div className="wrapper">
+        <div className="subHeaderBox">
+          <h2>Average Results for Average People</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <p>
+            Pellentesque convallis mauris urna, dictum efficitur eros finibus
+            dignissim.
+          </p>
+        </div>
+
+        <button className="getStartedButton">
+          <Link to={"/location"}>Get Started</Link>
+        </button>
       </div>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-      <p>
-        Pellentesque convallis mauris urna, dictum efficitur eros finibus
-        dignissim.
-      </p>
-      <button className="getStartedButton">
-        <Link to={"/location"}>Get Started</Link>
-      </button>
     </>
   );
 }
