01b6c5a2ae0421c5caed61999d4a85e768fd4160
ShannonTao <shannon.cxq@gmail.com>
2022-09-01 08:49:41 -0400

    removed darkContext

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import { DarkModeProvider } from "./components/DarkModeContext";
 
 import { Routes, Route } from "react-router-dom";
 
